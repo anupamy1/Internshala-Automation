f945982dc81c14c032ec7bb0bf2e09f33fbff884
anupamy1 <94732499+anupamy1@users.noreply.github.com>
2024-08-08 15:21:22 +0530

    secret.js

--- secret.js
+++ secret.js
@@ -1,6 +1,3 @@
 let id = "fosab59581@ddwfzp.com";
 let pass = "Cool@123";
 module.exports = {id, pass};
-// let id = "238kingslayer@gmail.com";
-// let pass = "Omveer1975@";
-// module.exports = {id, pass};
